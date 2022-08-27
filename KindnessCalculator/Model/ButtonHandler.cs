ccf540909cb05edb56accc91f9d2202e7cb8832e
Glads <Glads@DESKTOP-S6H9F2I>
2022-08-27 15:52:31 +0300

    Добавьте файлы проекта.

---
+++ KindnessCalculator/Model/ButtonHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace KindnessCalculator.Model
+{
+    public class ButtonHandler
+    {
+        private TextBox _resultTextBox;
+
+        public ButtonHandler(UIElementCollection layout)
+        {
+            _resultTextBox = layout[0] as TextBox;
+            foreach (UIElement element in layout)
+            {
+                Button button = element is Button ? element as Button : null;
+                if(button != null) button.Click += Button_Click;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)e.Source;
+            //double fisrtNum, lastNum;
+
+            if (_resultTextBox.Text[0] == '0' && button.Content.ToString() != "," && !_resultTextBox.Text.Contains(","))
+            {
+                _resultTextBox.Text = button.Content.ToString();
+            }
+            else
+            {
+                if (button.Content.ToString() == "," && _resultTextBox.Text.Contains(","))
+                    return;
+
+                _resultTextBox.Text += button.Content.ToString();
+            }
+
+        }
+    }
+}
---
+++ KindnessCalculator/View/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace KindnessCalculator.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new Model.ButtonHandler(Layout.Children);
+        }
+    }
+}
